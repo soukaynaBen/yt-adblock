94d573652baa28c415fa058f1a9f643ac4fef238
“soukaynaben” <soukabenlahbib@gmail.com>
2025-01-12 00:15:46 +0100

    first commit

---
+++ src/content.tsx
@@ -0,0 +1,23 @@
+import cssText from "data-text:~style.css"
+import type { PlasmoCSConfig } from "plasmo"
+
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://www.plasmo.com/*"]
+}
+
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
+
+const PlasmoOverlay = () => {
+  return (
+    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
+      hello
+    </div>
+  )
+}
+
+export default PlasmoOverlay
