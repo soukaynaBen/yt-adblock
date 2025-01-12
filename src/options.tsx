04279026019c2d55382521d92e81691d029b5b0c
“soukaynaben” <soukabenlahbib@gmail.com>
2025-01-12 16:50:34 +0100

    version 1.0.0

--- src/options.tsx
+++ src/options.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react"
 import "~style.css"
 
 function OptionsIndex() {
-  const [data, setData] = useState("")
 
   return (
     <div>
       <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+        Welcome to your yt-adblock Extension!
       </h1>
-      <h2>This is the Option UI page!</h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+ 
     </div>
   )
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,19 @@
 
 import "~style.css"
+import Logo from "react:~../assets/logo.svg"
+
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      hello
+    <div className="plasmo-flex plasmo-flex-col  plasmo-items-center plasmo-justify-center plasmo-h-52 plasmo-w-72 plasmo-p-8 plasmo-space-y-4">
+     <Logo  width={128}  className="plasmo-block" />
+
+       <h1 className=" plasmo-font-medium plasmo-capitalize plasmo-text-xl ">Youtube ads blocker</h1>
+      
     </div>
   )
 }
 
 export default IndexPopup
+
+
